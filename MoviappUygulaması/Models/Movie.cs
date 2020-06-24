2ca691bc4a5f633cfdb4fa67914db9548dee2e14
veysel gunes <veysel.gunes36@gmail.com>
2020-06-24 18:10:27 +0300

    first commit and controller and views page aded

---
+++ MoviappUygulaması/Models/Movie.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MoviappUygulaması.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Image { get; set; }
+        
+    }
+}
